dcc27ebb6caf18d2fb868d58706af6738527e6df
PedroHenriqueDevBR <p.lyma18@gmail.com>
2023-06-16 14:01:16 -0300

    chore: created production configuration

--- ldap_password/ldap_password/urls.py
+++ ldap_password/ldap_password/urls.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from apps.core import urls as core_urls
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(core_urls)),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns = urlpatterns + static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT,
+    )
+
+    urlpatterns = urlpatterns + static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
